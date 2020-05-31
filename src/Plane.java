44d039cdd39ee5eaeaa05b0e63551ea5f3d02846
Pedro <pedrocpt2001@yahoo.com>
2020-05-31 14:23:17 -0400

    added point and plane classes

---
+++ src/Plane.java
@@ -0,0 +1,10 @@
+public class Plane {
+
+    private Vector normalVector;
+    private Point point;
+
+    public Plane(Vector normalVector, Point point) {
+        this.normalVector = normalVector;
+        this.point = point;
+    }
+}
